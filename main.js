2d265dcf3af5982e9b938ca30f85883f17a0de30
Gustavo do Bem Ferreira <gustavo.bem@estudante.ufjf.br>
2023-12-11 11:59:53 -0300

    Adiciona na main import,  complementa função leFormulario e acrescenta limpaEstoque e atualizaTela

--- main.js
+++ main.js
@@ -1,16 +1,30 @@
 //main.js 
 
-document.entrada.addEventListener('submit', leFormulario);
-//leFormulario();
+import { limpaEstoque } from "./estoque.js";
 
 const olJoao = document.querySelector(#olJoao);
 const olMaria = document.querySelector(#olMaria);
+
+document.entrada.addEventListener('submit', leFormulario);
+//leFormulario();
+document.entrada.reset();
+
+document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('botaoLimparLista').addEventListener('click', () => {
+        limpaEstoque();
+        atualizaTela();
+    });
+});
+atualizaTela();
 
 function leFormulario(event) {
     event.preventDefault();
     const fruta = document.entrada.fruta.value;
     const quantidade = document.entrada.quantidade.value;
+    const origem = document.entrada.origem.value;
+    const destino = document.entrada.destino.value;
 
     console.log(`fruta: ${fruta} qtd: ${quantidade}`);
     //document.entrada.submit();
 }
+
